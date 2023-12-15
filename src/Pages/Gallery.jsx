34a3f4ffc2545ba473da6cd69f85cb9e027fb09f
pythitjolly <jolly.cusi74@gmail.com>
2023-12-15 14:12:18 +1300

    gallery

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -7,9 +7,9 @@ import "../Styles/Gallery.css";
 function Home() {
   return (
     <>
-    <h1><span style={{ fontWeight: "bold", color: "black", fontStyle: "italic", fontSize:"3rem", }}> Gallery</span></h1>
+    <h1><span style={{ fontWeight: "bold", color: "black", fontStyle: "italic", fontSize:"3rem",}}> Gallery</span></h1>
+    <p> <span style={{ fontWeight: "lighter", color: "black",  fontSize:"1.5rem", marginLeft:"12rem"}}>Check my works and find out why our customers keep on coming back. </span></p>
       <div className="gallery">
-        <section className="section4-Home"></section>
         <section className="section5-Gallery"></section>
         </div>
    
