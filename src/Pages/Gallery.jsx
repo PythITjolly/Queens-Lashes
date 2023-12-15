126d0c544f83278970004e209084f4e808ebf563
pythitjolly <jolly.cusi74@gmail.com>
2023-12-15 14:00:20 +1300

    Gallery header

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -7,6 +7,7 @@ import "../Styles/Gallery.css";
 function Home() {
   return (
     <>
+    <h1><span style={{ fontWeight: "bold", color: "black", fontStyle: "italic", fontSize:"3rem", }}> Gallery</span></h1>
       <div className="gallery">
         <section className="section4-Home"></section>
         <section className="section5-Gallery"></section>
